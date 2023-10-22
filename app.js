7c420bad9335ee85b161fb2d9a3304d352caa9c1
Ivan Glebov <125355249+IvanGlebov89@users.noreply.github.com>
2023-10-22 15:45:38 +0300

    Revert "Revert "удалил логи , добавил новые переменные""
    This reverts commit 1400ff83a05127efc5a181ef11d4c592bd47b4ec.

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-console.log("hi world");
-console.log("two");
-console.log(2 + 4);
-console.log(3 + 4);
-console.log("проверка");
-console.log(" coхроняет только через прогу -_-");
-const a = "hi world";
-console.log(a);
+let a = 5;
+let b = 5.6;
