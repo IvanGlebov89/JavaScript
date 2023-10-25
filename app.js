b0eebb1775dd21f211a9b5afd6202c99cfb65120
Ivan Glebov <125355249+IvanGlebov89@users.noreply.github.com>
2023-10-25 14:54:01 +0300

    преоброзование типов

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const projectHours = 40;
 const availableHours = (11 - 2) * 5;
 console.log("Смогу ли я работать " + (availableHours > projectHours));
 console.log("стоймость работы: " + payRateUSD * projectHours);
+
+// преобозование типов
+
+const age = "18";
+console.log(Number(age) + 5);
