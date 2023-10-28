cf0101aa1b74f6a8c856f9c11875a159c718309d
Ivan Glebov <125355249+IvanGlebov89@users.noreply.github.com>
2023-10-28 14:36:25 +0300

    promt и alert

--- app.js
+++ app.js
@@ -22,3 +22,27 @@ if (money >= product) {
     `не достаточно средств ! вам не хватило токенов ${product - money}`
   );
 }
+// задачка
+
+const deposit = 12000;
+const rate = 0.07;
+const depositLength = 24;
+const houseCost = 13500;
+const result = deposit * (1 + rate / 12) ** 24;
+
+if (result > houseCost) {
+  console.log(
+    `мы накопили ${result} можем купить дом. остаток средств ${
+      result - houseCost
+    }`
+  );
+} else {
+  console.log("купить не сможем");
+}
+
+const q = prompt("Введите число");
+if (Number(q) === 7) {
+  alert("вы угадали ");
+} else {
+  alert("вы не угадали");
+}
